bc31f14de8066c9e1d9670ce981635f5416a9058
Fernando Rodríguez <frr@keepcoding.io>
2022-02-04 13:04:34 +0100

    hasta find_streak

---
+++ linear_board.py
@@ -0,0 +1,42 @@
+from settings import BOARD_LENGTH
+
+class LinearBoard():
+    """
+    Clase que representa un tablero de una sola columna
+    x un jugador
+    o otro jugador
+    None un espacio vacío
+    """
+
+    def __init__(self):
+        """
+        Una lista de None
+        """
+        self._column = [None for i in range(BOARD_LENGTH)]
+
+    def add(self, char):
+        """
+        Juega en la primera posición disponible
+        """
+        # siempre y cunado no esté lleno...
+        if not self.is_full():
+            # buscamos la primera posición libre (None)
+            i = self._column.index(None)
+            # lo sustituimos por char
+            self._column[i] = char
+
+
+
+    def is_full(self):
+        return self._column[-1] != None
+
+
+    def is_victory(self, char):
+        return False
+
+    def is_tie(self, char1, char2):
+        """
+        no hay victoria ni de char1 ni de char 2
+        """
+        return (self.is_victory('x') == False) and (self.is_victory('o') == False)
+
---
+++ list_utils.py
@@ -0,0 +1,64 @@
+def find_one(list, needle):
+    """
+    Devuelve True si encuentra una o más ocurencias de needle en list
+    """
+    return find_n(list, needle, 1)
+
+
+def find_n(list, needle, n):
+    """
+    Devuelve True si en list hay n o más ocurrencias de needle
+    False si hay menos o si n < 0
+    """
+    # si n >= 0...
+    if n >= 0:
+        # Incializamos el índice y el contador
+        index = 0
+        count = 0
+
+        # mientras no hayamos encontrado al elemento n veces o no hayamos terminado la lista...
+        while count < n and index < len(list):
+            # si lo encontramos, actualizamos el contador
+            if needle == list[index]:
+                count = count + 1
+
+            # avanzamos al siguiente elemento
+            index = index + 1
+        # devolvemos el resultado de comparar contador con n
+        return count >= n
+    else:
+        return False
+
+def find_streak(list, needle, n):
+    """
+    Devuelve True si en list hay n o más needles seguidos
+    False, para todo lo demás
+    """
+    # si n >= 0
+    if n >= 0:
+        #Inicializo el indice, el contador y el indicador de racha
+        index = 0
+        count = 0
+        streak = False
+
+        # Mientras no haya encontradoa n seguidos u la lista no se haya acabao....
+        while count < n and index < len(list):
+            # si lo encuentro, activo el indicado de rachas y actualizo el contador
+            if needle == list[index]:
+                streak = True
+                count = count + 1
+            else:
+                # si no lo encuentro, desactivo el indicador de de racha y pongo a cero el contador
+                streak = False
+                count =0
+            
+            # avanzo al siguiente elemento
+            index = index + 1
+        
+        # devolvemos el resultado de comparar el contador con n SIEMPRE Y CUANDO  estemos en racha
+        return n >= count and streak
+    else:
+        # para valores de n < 0, no tiene sentido
+        return False
+    
+    
