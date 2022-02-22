7d08c52a27b5d4e5b78d3abbbbd5704ce629ee95
Fernando Rodríguez <frr@keepcoding.io>
2022-02-22 13:36:46 +0100

    human player

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from oracle import BaseOracle, ColumnClassification
+from oracle import BaseOracle, ColumnClassification, ColumnRecommendation
 
 
 class Player():
@@ -6,7 +6,7 @@ class Player():
     Juega en un tablero después de preguntar a un oráculo
     """
 
-    def __init__(self, name, char, oracle = BaseOracle()) -> None:
+    def __init__(self, name, char, oracle=BaseOracle()) -> None:
         self.name = name
         self.char = char
         self._oracle = oracle
@@ -15,23 +15,69 @@ class Player():
         """
         Elige la mejor columna de aquellas que recomienda el oráculo
         """
-        # obtén las recomendaciones
+        # Pregunto al oráculo
+        (best, recommendations) = self._ask_oracle(board)
+
+        # juego en la mejor
+        self._play_on(board, best.index)
+
+    def _play_on(self, board, position):
+        # juega en la pos
+        board.add(self.char, position)
+
+    def _ask_oracle(self, board):
+        """
+        Pregunta al oráculo y devuielve la mejor opción
+        """
+        # obtenemos las recommendaciones
         recommendations = self._oracle.get_recommendation(board, self)
 
-        # selecciona la mejor de todas
+        # seleccionamos la mejro
         best = self._choose(recommendations)
-        # juega en ella
-        board.add(self.char, best.index)
 
-
-
+        return (best, recommendations)
 
     def _choose(self, recommendations):
         # quitamos las no validas
-        valid = list(filter(lambda x : x.classification != ColumnClassification.FULL, recommendations))
+        valid = list(filter(lambda x: x.classification !=
+                     ColumnClassification.FULL, recommendations))
 
         # pillamos la primera de las válidas
         return valid[0]
-        
-        
 
+
+class HumanPlayer(Player):
+
+    def __init__(self, name, char):
+        super().__init__(name, char)
+
+    def _ask_oracle(self, board):
+        """
+        Le pido al humano que es mi oráculo
+        """
+        while True:
+            # pedimos columna al humano
+            raw = input('Select a column, puny human: ')
+            # verificamos que su respuesta no sea una idiotez
+            if _is_int(raw) and _is_within_column_range(board, int(raw)) and _is_non_full_column(board, int(raw)):
+
+                # si no lo es, jugamos donde ha dicho y salimos del bucle
+                pos = int(raw)
+                return (ColumnRecommendation(pos, None), None)
+
+
+# funciones de validación de índice de columna
+def _is_non_full_column(board, num):
+    return not board._columns[num].is_full()
+
+
+def _is_within_column_range(board, num):
+    return num >= 0 and num < len(board)
+
+
+def _is_int(aString):
+    try:
+        num = int(aString)
+        return True
+    except:
+        return False
--- player_test.py
+++ player_test.py
@@ -23,4 +23,32 @@ def test_play():
     assert before == after
 
 
+def test_valid_column():
+    board = SquareBoard.fromList([['x', None, None, None, ],
+                                  ['x', 'o', 'x', 'o', ],
+                                  ['o', 'o', 'x', 'x', ],
+                                  ['o', None, None, None, ]])
 
+    assert _is_within_column_range(board, 0)
+    assert _is_within_column_range(board, 1)
+    assert _is_within_column_range(board, 2)
+    assert _is_within_column_range(board, 3)
+    assert _is_within_column_range(board, 5) == False
+    assert _is_within_column_range(board, -10) == False
+    assert _is_within_column_range(board, 10) == False
+
+def test_is_non_full_column():
+    assert _is_non_full_column(board,0)
+    assert _is_non_full_column(board, 1) == False
+    assert _is_non_full_column(board,2) == False
+    assert _is_non_full_column(board, 3) 
+
+
+def test_is_int():
+    assert _is_int('42')
+    assert _is_int('0')
+    assert _is_int('-32')
+    assert _is_int('  32   ')
+    assert _is_int('hola') == False
+    assert _is_int('') == False
+    assert _is_int('3.14') == False
