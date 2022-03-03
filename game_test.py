7c570a3ae65412066e83752a99bf3806a64ef358
Fernando Rodríguez <frr@keepcoding.io>
2022-03-03 14:10:44 +0100

    test_is_game_over() is RED

--- game_test.py
+++ game_test.py
@@ -1,5 +1,7 @@
 import pytest
 from game import Game
+from square_board import SquareBoard
+
 
 def test_created_with_defaults():
     game = Game()
@@ -7,4 +9,39 @@ def test_created_with_defaults():
     assert game.match != None
     assert game.board != None
     assert game.board.is_full() == False
+    
+def test_is_game_over():
+    game = Game()
+    win_x = SquareBoard.fromList([['x', 'o', None, None, ],
+                                  ['o', 'x', None, None, ],
+                                  ['x', 'o', 'x', 'o', ],
+                                  ['x', 'o', None, None, ],
+                                  ])
+
+    win_o = SquareBoard.fromList([['x', 'o', 'x', 'o', ],
+                                  ['x', 'x', 'o', None, ],
+                                  ['o', 'o', None, None, ],
+                                  ['o', 'x', None, None, ]])
+
+    tie = SquareBoard.fromList([['o', 'x', 'x', 'o', ],
+                                ['x', 'o', 'o', 'x', ],
+                                ['o', 'x', 'x', 'o', ],
+                                ['x', 'o', 'o', 'x', ]])
+
+    unfinished = SquareBoard.fromList([['o', 'x', 'x', 'o', ],
+                                       [None, None, None, None, ],
+                                       [None, None, None, None, ],
+                                       [None, None, None, None, ]])
+
+    game.board = win_x
+    assert game._is_game_over() == True
+
+    game.board = win_o
+    assert game._is_game_over() == True
+
+    game.board = tie
+    assert game._is_game_over() == True
+
+    game.board = unfinished
+    assert game._is_game_over() == False
     
