bd5e7753d99b3a98ec0d97c4399599bded0db6d8
Fernando Rodríguez <frr@keepcoding.io>
2022-03-04 12:28:40 +0100

    implemented display_result

--- game.py
+++ game.py
@@ -44,7 +44,7 @@ class Game():
     def _start_game_loop(self):
         # bucle infinito
         while True:
-            #obtengo el juagdor de turno
+            # obtengo el juagdor de turno
             current_player = self.match.next_player
             # le hago jugar
             current_player.play(self.board)
@@ -52,31 +52,36 @@ class Game():
             self._display_move(current_player)
             # imprimo el tablero
             self._display_board()
-            #si el juego ha terminado,
+            # si el juego ha terminado,
             if self._is_game_over():
                 # muestro el resultado final
                 self._display_result()
-                #salgo del bucle
+                # salgo del bucle
                 break
 
     def _display_result(self):
-        pass
-    
+        winner = self.match.get_winner(self.board)
+        if winner != None:
+            print(f'\n{winner.name} ({winner.char}) wins!!!')
+        else:
+            print(
+                f'\nA tie between {self.match.get_player("x")} (x) and {self.match.get_player("o")} (o)!')
+
     def _is_game_over(self):
         """
         Game is over if there's a winner or there's a tie
         """
         winner = self.match.get_winner(self.board)
         if winner != None:
-            return True # there is a winner
+            return True  # there is a winner
         elif self.board.is_full():
-            return True # tie
+            return True  # tie
         else:
-            return False # the game is still on
-
+            return False  # the game is still on
 
     def _display_move(self, player):
-        print(f'\n{player.name} ({player.char}) has moved in column #{player.last_move}\n')
+        print(
+            f'\n{player.name} ({player.char}) has moved in column #{player.last_move}\n')
 
     def _display_board(self):
         """
@@ -84,20 +89,17 @@ class Game():
         """
         print(self.board)
 
-    
-    
-
     def _configure_by_user(self):
         """
         Pido al usuario valores deseados para match y tipo de partida
         """
-        
+
         # determino el tipo de partida (humano vs comp, comp vs comp)
         self.round_type = self._get_round_type()
 
         # Creamos los dos jugadores
         self.match = self._make_match()
-        
+
     def _make_match(self):
         """
         Player1 siempre será el ordeñador
@@ -111,10 +113,10 @@ class Game():
             # humano contro ordenador
             player1 = Player('T-800')
             player2 = HumanPlayer(name=input('Enter your name, puny human: '))
-        
+
         # creamos la partida
         return Match(player1, player2)
-        
+
     def _get_round_type(self):
         """
         Ask the user
